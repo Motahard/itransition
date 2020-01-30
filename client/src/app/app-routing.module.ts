0aedfaa8ff6617ced234c9c31d36a17ec2a54ff0
Motahard <mas280499@gmail.com>
2020-01-30 15:25:51 +0300

    created register and login pages ui

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { RegistrationPageComponent } from "./pages/registration-page/registration-page.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginPageComponent },
+  {
+    path: "register",
+    component: RegistrationPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,18 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
+  declarations: [AppComponent, LoginPageComponent, RegistrationPageComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
